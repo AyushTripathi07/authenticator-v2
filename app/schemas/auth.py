6285eb1ed5e94e937f311c2242e12bd89c1d9782
Ayush Tripathi <ayush@Ayushs-MacBook-Air-2.local>
2024-11-22 23:43:25 +0530

    Added QRCode schema

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -16,3 +16,6 @@ class LoginRequest(BaseModel):
     username: str = Field(..., min_length=3, max_length=50, example="john_doe")
     password: str = Field(..., min_length=8, example="SecurePassword123")
     otp: str = Field(..., min_length=6, max_length=6, example="123456")  
+
+class QRCodeRequest(BaseModel):
+    username: str
